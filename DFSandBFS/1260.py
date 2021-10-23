aea23d6d2ebf194d29f693608227a14ec7f89318
hanbinchoi <wkghskak@naver.com>
2021-10-23 20:33:46 +0900

    백준-BFS/DFS

--- DFSandBFS/1260.py
+++ DFSandBFS/1260.py
@@ -1,11 +1,31 @@
 from collections import deque
 
+# def dfs(graph, start, visited):
+#     visited[start] = True
+#     print(start, end=' ' )
+#     for i in graph[start]:
+#         if not visited[i]:
+#             dfs(graph,i,visited)
+#
+# def bfs(graph, start, visited):
+#     queue = deque([start])
+#     visited[start] = True
+#
+#     while queue:
+#         v = queue.popleft()
+#
+#         print(v, end=' ')
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+
 def dfs(graph, start, visited):
     visited[start] = True
-    print(start, end=' ' )
+    print(start,end=' ')
     for i in graph[start]:
         if not visited[i]:
-            dfs(graph,i,visited)
+            return dfs(graph, i, visited)
 
 def bfs(graph, start, visited):
     queue = deque([start])
@@ -14,11 +34,14 @@ def bfs(graph, start, visited):
     while queue:
         v = queue.popleft()
 
-        print(v, end=' ')
+        print(v, end=" ")
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
+
+    return
+
 point, node, start = map(int,input().split())
 graph = [[] for _ in range(point+1)]
 
@@ -28,6 +51,7 @@ for i in range(node):
     graph[y].append(x)
     graph[x].sort()
 visited = [False] * (point+1)
+print(graph)
 dfs(graph,start,visited)
 visited = [False] * (point+1)
 print()
