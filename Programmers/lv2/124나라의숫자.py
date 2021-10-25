2d4589df7188f9ba600b24029909e35ad7531e84
hanbinchoi <wkghskak@naver.com>
2021-10-25 23:23:59 +0900

    Programmers lv2

--- Programmers/lv2/124나라의숫자.py
+++ Programmers/lv2/124나라의숫자.py
@@ -1,16 +1,27 @@
 def solution(n):
     answer = ''
     a,b = n//3, n%3
-    if b==0:
-        a -= 1
+    add=""
 
     while a>=4:
+        if b == 0:
+            a -= 1
+            b=4
+        add += str(b)
+        k = a
+        a = a//3
+        b = k%3
 
     if b==0:
         a -= 1
         b = 4
     if a==3:
         a=4
-    return str(a)+str(b)
+    if a==-1:
+        a=0
+    answer += str(a)+str(b)+add
+    answer = answer.replace("0","")
+    return answer
 
-print(solution(13))
+for i in range(100):
+    print(i,"=",solution(i))
