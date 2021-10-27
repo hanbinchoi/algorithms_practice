f020c7029a5bdd6c3ca703f905465a76a13d7e69
hanbinchoi <wkghskak@naver.com>
2021-10-27 20:08:42 +0900

    Programmers lv2

---
+++ Programmers/lv2/타겟넘버.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp+(-1*numbers[idx]), idx])
+        else:
+            if temp == target:
+                answer+=1
+    return answer
+print(solution([1, 1, 1, 1, 1],	3))
