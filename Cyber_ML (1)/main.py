826fe86ed30167efd8373e7054bd684bbb8a2439
hanbinchoi <wkghskak@naver.com>
2021-11-28 19:38:32 +0900

    cs

--- Cyber_ML (1)/main.py
+++ Cyber_ML (1)/main.py
@@ -9,80 +9,156 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+#
+#
+# data =pd.read_csv("cyber_data.csv")
+#
+# feature_list=["file_name","num_PID","PID","Parent PID","Property","RelationShip","Path","Protocol","Address"]
+#
+# data=data.loc[:,feature_list]
+#
+#
+# #결측값 제거 임시
+#
+# data=data.fillna(method='ffill')
+# data=data.fillna(method='bfill')
+#
+#
+# #숫자 타입으로 변경 (encoding)
+#
+# encoder = LabelEncoder()
+#
+# encoder.fit(data["file_name"])
+# data["file_name"] = pd.DataFrame(encoder.transform(data["file_name"]))
+#
+# encoder.fit(data["PID"])
+# data["PID"] =pd.DataFrame(encoder.transform(data["PID"]))
+#
+# encoder.fit(data["RelationShip"])
+# data["RelationShip"] =pd.DataFrame(encoder.transform(data["RelationShip"]))
+#
+# encoder.fit(data["Path"])
+# data["Path"] =pd.DataFrame(encoder.transform(data["Path"]))
+#
+# encoder.fit(data["Protocol"])
+# data["Protocol"] =pd.DataFrame(encoder.transform(data["Protocol"]))
+#
+# encoder.fit(data["Address"])
+# data["Address"] =pd.DataFrame(encoder.transform(data["Address"]))
+#
+#
+# # 추가 -> Min-max Normalization
+# f = data[["num_PID","Property"]]
+#
+# min_max_scaler = MinMaxScaler()
+# fitted = min_max_scaler.fit(f)
+#
+# f = min_max_scaler.transform(f)
+# f = pd.DataFrame(f,columns=["num_PID","Property"])
+#
+#
+# data = data.drop(["num_PID","Property"],axis=1)
+# data[["num_PID","Property"]] = f[["num_PID","Property"]]
+#
+#
+# #target 임시 (Parent PID)
+# X= data.drop(columns=['Parent PID'])
+# y=data['Parent PID']
+#
+# #train, test set = 8 : 2
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#
+# #decision Tree
+# decision = DecisionTreeClassifier(criterion='entropy', max_depth=15, random_state=30)
+# decision.fit(X_train, y_train)
+# y_pred = decision.predict(X_test)
+#
+# print("accuracy: %.3f" %accuracy_score(y_test, y_pred))
+#
+# # confusion matrix
+# print('[Decision Tree]')
+# confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'], margins= True)
+# sns.heatmap(confusion_matrix, annot=True, fmt = 'd')
+# plt.show()
+#
+# # classification report
+# print(classification_report(y_test, y_pred))
+#
+import mlxtend
+import numpy as np
+import pandas as pd
 
-
+# 데이터 불러오기
 data =pd.read_csv("cyber_data.csv")
-
-feature_list=["file_name","num_PID","PID","Parent PID","Property","RelationShip","Path","Protocol","Address"]
-
+feature_list=["num_PID","PID","Parent PID","Property","RelationShip","Protocol","Address","Port","UsedFile","family"]
 data=data.loc[:,feature_list]
 
+# 결측값 제거
+data = data.fillna({"RelationShip":"no-relation"})
+data = data.fillna({"Property":0})
+data = data.fillna({"Address":"no-addr"})
 
-#결측값 제거 임시
+# num_PID, property 정규화
+def pid_normalization(x):
+    if x >= 200:
+        return "pid_num:200"
+    elif x >=100:
+        return "pid_num:100"
+    elif x >=80:
+        return "pid_num:80"
+    elif x >=50:
+        return "pid_num:50"
+    elif x >=30:
+        return "pid_num:30"
+    elif x >=20:
+        return "pid_num:20"
+    elif x >=10:
+        return "pid_num:10"
+    else:
+        return "pid_num:0"
 
-data=data.fillna(method='ffill')
-data=data.fillna(method='bfill')
+def prop_normalization(x):
+    if x >= 200:
+        return "property:200"
+    elif x >=100:
+        return "property:100"
+    elif x >=80:
+        return "property:80"
+    elif x >=50:
+        return "property:50"
+    elif x >=30:
+        return "property:30"
+    elif x >=20:
+        return "property:20"
+    elif x >=10:
+        return "property:10"
+    elif x >=5:
+        return "property:5"
+    else:
+        return "property:0"
 
+data["num_PID"] = data["num_PID"].apply(pid_normalization)
+data["Property"] = data["Property"].apply(prop_normalization)
+print(data.info())
 
-#숫자 타입으로 변경 (encoding)
+def paret_PID_str(x):
+    return "Parent PID:"+str(x)
+def port_str(x):
+    return "Port:"+str(x)
+def usedFile_str(x):
+    return "UsedFile:"+str(x)
 
-encoder = LabelEncoder()
+# 모든 컬럼 문자열로 변경
+data["Parent PID"] = data["Parent PID"].apply(paret_PID_str)
+data["Port"] = data["Port"].apply(port_str)
+data["UsedFile"] = data["UsedFile"].apply(usedFile_str)
 
-encoder.fit(data["file_name"])
-data["file_name"] = pd.DataFrame(encoder.transform(data["file_name"]))
+data = data.to_numpy()
+from mlxtend.preprocessing import TransactionEncoder
+te = TransactionEncoder()
+te_ary = te.fit(data).transform(data)
+df = pd.DataFrame(te_ary, columns=te.columns_)
 
-encoder.fit(data["PID"])
-data["PID"] =pd.DataFrame(encoder.transform(data["PID"]))
+from mlxtend.frequent_patterns import fpgrowth
 
-encoder.fit(data["RelationShip"])
-data["RelationShip"] =pd.DataFrame(encoder.transform(data["RelationShip"]))
-
-encoder.fit(data["Path"])
-data["Path"] =pd.DataFrame(encoder.transform(data["Path"]))
-
-encoder.fit(data["Protocol"])
-data["Protocol"] =pd.DataFrame(encoder.transform(data["Protocol"]))
-
-encoder.fit(data["Address"])
-data["Address"] =pd.DataFrame(encoder.transform(data["Address"]))
-
-
-# 추가 -> Min-max Normalization
-f = data[["num_PID","Property"]]
-
-min_max_scaler = MinMaxScaler()
-fitted = min_max_scaler.fit(f)
-
-f = min_max_scaler.transform(f)
-f = pd.DataFrame(f,columns=["num_PID","Property"])
-
-
-data = data.drop(["num_PID","Property"],axis=1)
-data[["num_PID","Property"]] = f[["num_PID","Property"]]
-
-
-#target 임시 (Parent PID)
-X= data.drop(columns=['Parent PID'])
-y=data['Parent PID']
-
-#train, test set = 8 : 2
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-#decision Tree
-decision = DecisionTreeClassifier(criterion='entropy', max_depth=15, random_state=30)
-decision.fit(X_train, y_train)
-y_pred = decision.predict(X_test)
-
-print("accuracy: %.3f" %accuracy_score(y_test, y_pred))
-
-# confusion matrix
-print('[Decision Tree]')
-confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'], margins= True)
-sns.heatmap(confusion_matrix, annot=True, fmt = 'd')
-plt.show()
-
-# classification report
-print(classification_report(y_test, y_pred))
-
-
-
+print(fpgrowth(df, min_support=0.2, use_colnames=True))
---
+++ Cyber_ML (1)/main2.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+
+data =pd.read_csv("cyber_data.csv")
+
+feature_list=["file_name","num_PID","Parent PID","PID","RelationShip","Path","Address","Protocol","Property","Port","UsedFile","family","hash_value"]
+
+data=data.loc[:,feature_list]
+
+
+#결측값 제거
+
+data["Property"]=data["Property"].fillna(0)
+data["RelationShip"]=data["RelationShip"].fillna("relation_empty")
+data["Path"]=data["Path"].fillna("Path_empty")
+data["Address"]=data["Address"].fillna("0.0.0.0")
+
+# Property, num_PID 정규화(추가 부분)
+def pid_normalization(x):
+    if x >= 200:
+        return 200
+    elif x >=100:
+        return 100
+    elif x >=80:
+        return 80
+    elif x >=50:
+        return 50
+    elif x >=30:
+        return 20
+    elif x >=20:
+        return 10
+    elif x >=10:
+        return 10
+    else:
+        return 0
+
+def prop_normalization(x):
+    if x >= 200:
+        return 200
+    elif x >=100:
+        return 100
+    elif x >=80:
+        return 80
+    elif x >=50:
+        return 50
+    elif x >=30:
+        return 30
+    elif x >=20:
+        return 20
+    elif x >=10:
+        return 10
+    elif x >=5:
+        return 5
+    else:
+        return 0
+
+data["num_PID"] = data["num_PID"].apply(pid_normalization)
+data["Property"] = data["Property"].apply(prop_normalization)
+
+#item set
+itemset_size = len(data)
+itemset_feature =["num_PID","Parent PID","PID","Path","RelationShip","Address","Protocol","Property","Port","UsedFile","family","hash_value"]
+item_frequent= dict()
+itemset=[0 for i in range(itemset_size)]
+for i in range(0,itemset_size):
+    each_list =[0 for i in range(len(itemset_feature))]
+    for j in range(0, len(itemset_feature)):
+        # item frequency
+        if data[itemset_feature[j]][i] in item_frequent:
+            value = item_frequent[data[itemset_feature[j]][i]]
+            item_frequent[data[itemset_feature[j]][i]]=value+1
+        else :
+            item_frequent[data[itemset_feature[j]][i]]=1
+        each_list[j]=data[itemset_feature[j]][i]
+    itemset[i]=each_list
+
+
+
+#delete under threshold
+threshold =2
+for i in range(0,len(itemset)):
+    for j in range(0,len(itemset[i])):
+        if(item_frequent[itemset[i][j]]<threshold):
+            itemset[i][j]=-1
+
+for i in range(0,len(itemset)):
+    while(-1 in itemset[i]):
+        itemset[i].remove(-1)
+
+keylist = item_frequent.keys()
+
+
+
+#(ordered) item_frequency
+sorted(item_frequent.items(),reverse=True, key=lambda item:item[1])
+
+
+
+for i in range(0, len(itemset)):
+    #sorting
+    index=0
+    for j in range(0, len(itemset[i])):
+        for k in range(j, len(itemset[i])):
+            if(item_frequent[itemset[i][j]]<item_frequent[itemset[i][k]]):
+                temp = itemset[i][j]
+                itemset[i][j]=itemset[i][k]
+                itemset[i][k]=temp
+
+
+
+
+#convert to str
+for i in range(0,len(itemset)):
+    for j in range(0, len(itemset[i])):
+        itemset[i][j]=str(itemset[i][j])
+
+
+# FP-growth
+data = np.array(itemset)
+from mlxtend.preprocessing import TransactionEncoder
+te = TransactionEncoder()
+te_ary = te.fit(data).transform(data)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+
+from mlxtend.frequent_patterns import fpgrowth
+
+fpgrowth_result=fpgrowth(df, min_support=0.2, use_colnames=True)
+
+print("Result FP-growth")
+print(fpgrowth_result)
+
+
+#Apriori
+from mlxtend.frequent_patterns import apriori
+apriori_result=apriori(df, min_support=0.3, use_colnames=True)
+print("\nResult Apriori ")
+print(apriori_result)
